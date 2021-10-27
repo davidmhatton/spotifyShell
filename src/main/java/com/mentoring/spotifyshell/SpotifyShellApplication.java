9b9b74c94300d0a7966c84c680f03b878ec7ca37
David Hatton <dhatton@turnitin.com>
2021-10-27 21:04:24 +0100

    Add initial commit - project init

---
+++ src/main/java/com/mentoring/spotifyshell/SpotifyShellApplication.java
@@ -0,0 +1,13 @@
+package com.mentoring.spotifyshell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyShellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyShellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mentoring/spotifyshell/SpotifyShellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentoring.spotifyshell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyShellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
