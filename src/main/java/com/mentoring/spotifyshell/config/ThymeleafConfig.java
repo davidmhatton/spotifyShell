e43f3c6a7b44695abe1a7612dcd1b19acee71774
David Hatton <dhatton@turnitin.com>
2021-11-03 12:09:15 +0000

    main page
    had to add various configs as thymeleaf for spring boot wasn't in the classpath, and also some config was required to allow us to add external stylesheets / js files to thymeleaf pages.

---
+++ src/main/java/com/mentoring/spotifyshell/config/ThymeleafConfig.java
@@ -0,0 +1,23 @@
+package com.mentoring.spotifyshell.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class ThymeleafConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                "/img/**",
+                "/css/**",
+                "/js/**")
+                .addResourceLocations(
+                        "classpath:/static/img/",
+                        "classpath:/static/css/",
+                        "classpath:/static/js/");
+    }
+}
